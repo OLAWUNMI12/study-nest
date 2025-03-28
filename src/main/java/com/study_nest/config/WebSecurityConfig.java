559e56b04a4583442a47429afeafa1935ab586ad
Moshood Aniya <moshood.aniya@moniepoint.com>
2025-03-28 01:07:39 +0100

    Big fix

--- src/main/java/com/study_nest/config/WebSecurityConfig.java
+++ src/main/java/com/study_nest/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:665e6c28c8ca3aae17479f6a21dde796cb601d7b1a7a494d413f83c076525dd7
-size 2310
+oid sha256:6ab95197278d8bc3cc687c115c94f029e149bce015ddc71f5f71c24bf5d577f7
+size 2769
